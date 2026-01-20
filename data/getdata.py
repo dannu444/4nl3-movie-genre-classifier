f8891d38a4530ef443539c2023ed595be264d87c
Nour Fawaz <nourfawaz4321@gmail.com>
2026-01-19 23:31:37 -0500

    fixed empty rows in table

--- data/getdata.py
+++ data/getdata.py
@@ -7,7 +7,7 @@ tmdb_api_key = "a8ebfa44b6e96530303358f42c0c8287"
 data_dir = "./data"
 
 #load data
-number_of_rows = 1000
+number_of_rows = 5500
 df_movies = pd.read_csv(f"{data_dir}/ml-20m/movies.csv", nrows=number_of_rows)
 df_links = pd.read_csv(f"{data_dir}/ml-20m/links.csv", nrows=number_of_rows)
 
@@ -32,6 +32,7 @@ def fetch_movie_data(tmdb_id):
 
 # fetch plots and adjust genres to only one genre
 for index, row in df_combined.iterrows():
+    print(f"Processing row {index+1}")
     tmdb_id = row['tmdbId']
     plot = fetch_movie_data(tmdb_id) # fetch movie plot 
     if plot:
@@ -39,8 +40,9 @@ for index, row in df_combined.iterrows():
     df_combined.at[index, 'genres'] = row['genres'].split('|')[0]  # adjust to keep only first genre
     time.sleep(0.25)  # for TMDb rate limit
 
-# drop rows with missing plots and remove unnecessary columns
-df_combined.dropna(subset=['plot'], inplace=True)
+# drop rows with missing values and remove unnecessary columns
+df_combined.dropna(inplace=True)
+df_combined = df_combined.dropna(subset=['plot'])
 df_combined = df_combined[['movieId', 'title', 'genres', 'plot']]
 print(f"Dataframe shape after fetching plots and cleaning: {df_combined.shape}")
 
